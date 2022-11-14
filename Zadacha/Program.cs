00d0316a07273e51d953d03d45e0a3a4e04753e3
Roman <79613377185R@gmail.com>
2022-11-14 23:58:10 +0300

    init

---
+++ Zadacha/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Zadayte dlinnu massiva!");
+int DlnnaMassiva = Convert.ToInt32(Console.ReadLine());
+
+string [] getarray1 (int DlnnaMassiva)
+{
+    string[] array1= new string [DlnnaMassiva];
+    for (int i = 0; i < DlnnaMassiva; i++)
+    {
+        Console.WriteLine("vvedite strokovoe znachenie cherez (,) v covychkah ");
+        array1[i] = Console.ReadLine();
+    }
+
+    return array1;
+}
+
+string [] array1 = getarray1 (DlnnaMassiva);
+Console.WriteLine(array1);
