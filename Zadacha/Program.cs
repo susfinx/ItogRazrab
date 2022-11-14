8616a9a3e93d805d69da065d6cc473e7058e8a0a
Roman <79613377185R@gmail.com>
2022-11-15 01:46:02 +0300

    Resheno

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,8 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Zadayte dlinnu massiva!");
-int DlnnaMassiva = Convert.ToInt32(Console.ReadLine());
-
 string [] getarray1 (int DlnnaMassiva)
 {
     string[] array1= new string [DlnnaMassiva];
@@ -15,6 +12,24 @@ string [] getarray1 (int DlnnaMassiva)
     return array1;
 }
 
+string [] getarray2 (string [] array1)
+{
+    
+    string [] array2 = new string [array1.Length];
+    int j =0;
+    int k = 3;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length<=k)
+        {
+            array2[j]=array1[i];
+            j++;
+        }
+    } 
+
+    return array2;
+}
+
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -22,9 +37,15 @@ void PrintArray(string[] array)
         Console.Write($"{array[i]} ");
     }
      Console.WriteLine();
- }
+}
 
+Console.WriteLine("Zadayte dlinnu massiva!");
+int DlnnaMassiva = Convert.ToInt32(Console.ReadLine());
 string [] array1 = getarray1 (DlnnaMassiva);
+string [] array2 = getarray2 (array1);
+Console.WriteLine();
 Console.WriteLine ("Vvedenyi Massiv");
 PrintArray (array1);
+Console.WriteLine();
 Console.WriteLine("massiv po usloviyam zadachi");
+PrintArray (array2);
